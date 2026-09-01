73a844f0e975b743286c2ab76957ca34cc3f387c
demainpaulogifted <demainpaulogifted@outlook.com>
2026-09-01 17:55:32 +0100

    Create components/Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+
+      <div className="footer-brand">
+        <span className="brand-icon small">P</span>
+        <span>Pitnex</span>
+      </div>
+
+      <p>© 2026 Pitnex. All rights reserved.</p>
+
+    </footer>
+  );
+}
